7ccd1c568a94a85c34874216a5e2dde281dfd72c
AngelinaCanguenha <116636549+AngelinaCanguenha@users.noreply.github.com>
2023-06-19 13:27:10 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ###### In this notebook, Data Science Tools and Ecosystem are summarized about languages, libraries and repository that we can use for Data Science. After completing this course, we will be able to manege and type a basic programing language code, and to know the many libriaries and data science tools.
+
+# ### <b> Objectives:</b>
+# 
+
+# ##### List popular languages for Data Science
+# - Popular Languages
+# - Popular Libraries
+# - data Science Tools
+
+# ##### Some of the popular languages that Data Scientists use are:
+# <ol> 
+#     <li> Phython </li>
+#     <li> R </li>
+#     <li> Matlab </li>
+#     <li> Java </li>
+#     </ol>
+
+# ###### Some of the commonly used libraries used by Data Scientists include:
+# 
+
+# <ol>
+#     <li> Pandas </li>
+#     <li> Numpy </li>
+#     <li> ggplot</li>
+#     <li> Matplotlib </li>
+#     </ol>
+
+# <table>
+#     <tr>
+#         <th> Data Science Tools</th>
+#     </tr>
+#     <tr>
+#         <td> Anaconda</td>
+#     </tr>
+#     <tr>
+#         <td> Visual Studio </td>
+#     </tr>
+#     <tr>
+#         <td> Apache </td>
+#     </tr>
+#     </table>
+#             
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+(3*4)+5
+
+
+# ###### This will convert 200 minutes to hours by diving by 60
+# 
+# 
+
+# In[12]:
+
+
+200/60
+
+
+
+# ## Author
+# Angelina Napende Canguenha
+
+# In[ ]:
+
+
+
+
